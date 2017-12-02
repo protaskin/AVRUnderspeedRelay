760d6e88176313d897c28d5f8ac549881b363b4f
Artyom Protaskin <a.protaskin@gmail.com>
2017-12-03 01:53:35 +0300

    Replace tabs with spaces in Python code (PEP 8)

--- Prototypes/calc_delay_ticks.py
+++ Prototypes/calc_delay_ticks.py
@@ -17,20 +17,20 @@ MIN_DELAY_TICKS = 100
 MAX_DELAY_TICKS = 1000
 
 def calc_delay_ticks(adc_value):
-	return (MIN_DELAY_TICKS + (adc_value << 8) /
-		((UINT8_MAX << 8) / (MAX_DELAY_TICKS - MIN_DELAY_TICKS)))
+    return (MIN_DELAY_TICKS + (adc_value << 8) /
+        ((UINT8_MAX << 8) / (MAX_DELAY_TICKS - MIN_DELAY_TICKS)))
 
 def accy_calc_delay_ticks(adc_value):
-	return (MIN_DELAY_TICKS +
-		(float(adc_value) / 255) * (MAX_DELAY_TICKS - MIN_DELAY_TICKS))
+    return (MIN_DELAY_TICKS +
+        (float(adc_value) / 255) * (MAX_DELAY_TICKS - MIN_DELAY_TICKS))
 
 for adc_value in range(256):
-	ticks = calc_delay_ticks(adc_value)
-	accy_ticks = accy_calc_delay_ticks(adc_value)
-	error = 100 * math.fabs(accy_ticks - ticks) / accy_ticks
-	print('adc_value: %d; ticks: %d (approx), %.3f (accy); error: %.3f%%'
-		% (adc_value, ticks, accy_ticks, error))
-	if error > 1:
-		print('WARNING: The error is greater than 1%')
-	if adc_value and calc_delay_ticks(adc_value - 1) > ticks:
-		print('WARNING: The value is less than the previous')
+    ticks = calc_delay_ticks(adc_value)
+    accy_ticks = accy_calc_delay_ticks(adc_value)
+    error = 100 * math.fabs(accy_ticks - ticks) / accy_ticks
+    print('adc_value: %d; ticks: %d (approx), %.3f (accy); error: %.3f%%'
+        % (adc_value, ticks, accy_ticks, error))
+    if error > 1:
+        print('WARNING: The error is greater than 1%')
+    if adc_value and calc_delay_ticks(adc_value - 1) > ticks:
+        print('WARNING: The value is less than the previous')
--- Prototypes/calc_target_ticks.py
+++ Prototypes/calc_target_ticks.py
@@ -19,35 +19,35 @@ ref_points = [0, 6, 12, 20, 28, 39, 51, 67, 89, 128]
 REF_POINTS_SIZE = 9 # The last element of the ref_points list is not counted
 
 def calc_target_ticks(adc_value):
-	if adc_value >= 0x80:
-		adc_value -= 0x80
-		rval = MID_TARGET_TICKS
-		step = (MID_TARGET_TICKS - MAX_TARGET_TICKS) / REF_POINTS_SIZE
-	else:
-		rval = MIN_TARGET_TICKS
-		step = (MIN_TARGET_TICKS - MID_TARGET_TICKS) / REF_POINTS_SIZE
+    if adc_value >= 0x80:
+        adc_value -= 0x80
+        rval = MID_TARGET_TICKS
+        step = (MID_TARGET_TICKS - MAX_TARGET_TICKS) / REF_POINTS_SIZE
+    else:
+        rval = MIN_TARGET_TICKS
+        step = (MIN_TARGET_TICKS - MID_TARGET_TICKS) / REF_POINTS_SIZE
 
-	for i in reversed(range(REF_POINTS_SIZE)): # 8..0
-		if adc_value >= ref_points[i]:
-			adc_value -= ref_points[i]
-			rval -= i * step
-			if adc_value:
-				rval -= int((step * adc_value) /
-					(ref_points[i + 1] - ref_points[i])) # The last element of the ref_points list is used only here
-			break
+    for i in reversed(range(REF_POINTS_SIZE)): # 8..0
+        if adc_value >= ref_points[i]:
+            adc_value -= ref_points[i]
+            rval -= i * step
+            if adc_value:
+                rval -= int((step * adc_value) /
+                    (ref_points[i + 1] - ref_points[i])) # The last element of the ref_points list is used only here
+            break
 
-	return rval
+    return rval
 
 def accy_calc_target_ticks(adc_value):
-	return pow(10, (3 - float(adc_value) / 128))
+    return pow(10, (3 - float(adc_value) / 128))
 
 for adc_value in range(256):
-	ticks = calc_target_ticks(adc_value)
-	accy_ticks = accy_calc_target_ticks(adc_value)
-	error = 100 * math.fabs(accy_ticks - ticks) / accy_ticks
-	print('adc_value: %d; ticks: %d (approx), %.3f (accy); error: %.3f%%'
-		% (adc_value, ticks, accy_ticks, error))
-	if error > 15:
-		print('WARNING: The error is greater than 15%')
-	if adc_value and calc_target_ticks(adc_value - 1) < ticks:
-		print('WARNING: The value is greater than the previous')
+    ticks = calc_target_ticks(adc_value)
+    accy_ticks = accy_calc_target_ticks(adc_value)
+    error = 100 * math.fabs(accy_ticks - ticks) / accy_ticks
+    print('adc_value: %d; ticks: %d (approx), %.3f (accy); error: %.3f%%'
+        % (adc_value, ticks, accy_ticks, error))
+    if error > 15:
+        print('WARNING: The error is greater than 15%')
+    if adc_value and calc_target_ticks(adc_value - 1) < ticks:
+        print('WARNING: The value is greater than the previous')
